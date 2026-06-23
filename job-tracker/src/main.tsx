516403b8a1247e2d2c1f5351c7843491b92f2855
Suraj Thapa <surajthapa7787@gmail.com>
2026-06-23 15:39:07 +0530

    implemented persist and  ✨ new dependencies optimized: redux-persist/es/storage

--- job-tracker/src/main.tsx
+++ job-tracker/src/main.tsx
@@ -6,15 +6,19 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
+        <PersistGate loading={null} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>,
--- job-tracker/src/store/store.ts
+++ job-tracker/src/store/store.ts
@@ -1,9 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobReducer from "../features/jobs/jobsSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, jobReducer);
+
 export const store = configureStore({
   reducer: {
-    jobs: jobReducer,
+    jobs: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
