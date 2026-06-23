38eb0d9d943be2efc160990e5533cb9f5e15ccd4
Suraj Thapa <surajthapa7787@gmail.com>
2026-06-23 10:47:39 +0530

    created jobfilter functionality

---
+++ job-tracker/src/features/jobs/components/JobFilters.tsx
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { JobStatus } from "../types";
+import type { RootState } from "../../../store/store";
+import { setFilter, setSearchQuery } from "../jobsSlice";
+
+function JobFilters() {
+  const dispatch = useDispatch();
+
+  const Filters: Array<JobStatus | "all"> = [
+    "all",
+    "applied",
+    "interview",
+    "offer",
+    "rejected",
+  ];
+  const currentFilter = useSelector((state: RootState) => state.jobs.filter);
+  const currentSearchQuery = useSelector(
+    (state: RootState) => state.jobs.searchQuery,
+  );
+  return (
+    <div>
+      <div>
+        {" "}
+        <input
+          type="text"
+          value={currentSearchQuery}
+          onChange={(e) => dispatch(setSearchQuery(e.target.value))}
+          placeholder="search job you applied"
+        />
+      </div>
+      <div>
+        {Filters.map((filter) => {
+          return (
+            <button
+              className={`rounded-full px-5 py-2 text-sm font-semibold capitalize transition ${
+                currentFilter === filter
+                  ? "bg-blue-600 text-white" // <-- Highlighting the active button!
+                  : "bg-slate-900 text-slate-400 hover:bg-slate-800 hover:text-slate-200" // <-- Inactive buttons
+              }`}
+              key={filter}
+              onClick={() => dispatch(setFilter(filter))}
+            >
+              {filter}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default JobFilters;
